7602f89e2603b02f612a08c76758964691ee03dc
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-08 00:46:13 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,25 @@
 class Transfer
-  # your code here
+  
+  attr_accessor :sender, :receiver, :status, :amount 
+  
+  def initialize(sender, receiver, amount) 
+    @sender = sender 
+    @receiver = receiver 
+    @status = "pending" 
+    @amount = amount  
+  end 
+  
+  def valid? 
+    if self.valid? == true && BankAccount.valid? == true 
+      true 
+    else 
+      false 
+    end 
+  end 
+  
+  def execute_transaction
+  end 
+  
+  def reverse_transfer 
+  end 
 end
