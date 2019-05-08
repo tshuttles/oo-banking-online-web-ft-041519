b87b3c82256e43b0f71d2a8599773c0eb4da27bd
Tachi Shuttlesworth <tshuttlesworth@gmail.com>
2019-05-08 03:13:52 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,8 +15,8 @@ class Transfer
   
   def execute_transaction
     if @status == "pending" && @sender.balance > amount 
-      @sender.balance -= @amount 
-      @receiver.balance += @amount 
+      @sender.balance -= amount 
+      @receiver.balance += amount 
       @status = "complete"
     else 
       @status = "rejected" 
